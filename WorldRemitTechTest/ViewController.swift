a3632bff820537e3b6a5256b6ec4351f8eaec947
Sebastian Skuse <seb@skus.es>
2019-09-23 09:45:27 +0100

    Initial Commit

---
+++ WorldRemitTechTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorldRemitTechTest
+//
+//  Created by Seb Skuse on 23/09/2019.
+//  Copyright © 2019 Seb Skuse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WorldRemitTechTestTests/WorldRemitTechTestTests.swift
@@ -0,0 +1,34 @@
+//
+//  WorldRemitTechTestTests.swift
+//  WorldRemitTechTestTests
+//
+//  Created by Seb Skuse on 23/09/2019.
+//  Copyright © 2019 Seb Skuse. All rights reserved.
+//
+
+import XCTest
+@testable import WorldRemitTechTest
+
+class WorldRemitTechTestTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
